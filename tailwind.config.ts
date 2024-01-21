32b2a6d30ae579700a9bb9021bdac23d7bd577ad
Dylan van Heerden <61427891+dylanvanh@users.noreply.github.com>
2024-01-21 22:34:45 +0200

    feat: husky, commitlint, tailwind merge utils , tailwind theme (#2)
    * remove boilerplate, add prettier, add tailwind
    
    * fix: prettier, eslint
    
    * chore: add husky & commitlint
    
    * feat: tailwind theme
    
    * feat: tailwind merge

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,37 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {}
-	},
-	plugins: []
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+        heading: ["var(--font-heading)", ...fontFamily.sans],
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
+  plugins: [],
+};
